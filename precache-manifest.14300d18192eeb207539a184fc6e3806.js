7b69bd92791bea53b2c5aeb31fa2c24ff364507f
hao-nhien.nguyen <hao-nhien.nguyen@epitech.eu>
2020-07-10 22:51:34 +0200

    Updates

---
+++ precache-manifest.14300d18192eeb207539a184fc6e3806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a6f7841a0a15c3f5b53dad3707c929",
+    "url": "/my_snapchat/index.html"
+  },
+  {
+    "revision": "9501bdc8326cbab715a7",
+    "url": "/my_snapchat/static/css/main.abe5db80.chunk.css"
+  },
+  {
+    "revision": "1476b4467d515276900c",
+    "url": "/my_snapchat/static/js/2.5038f8a1.chunk.js"
+  },
+  {
+    "revision": "e385af4024d7f6454ef5f7e8e658b6ca",
+    "url": "/my_snapchat/static/js/2.5038f8a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9501bdc8326cbab715a7",
+    "url": "/my_snapchat/static/js/main.3942e8af.chunk.js"
+  },
+  {
+    "revision": "bd98006596306c89779e",
+    "url": "/my_snapchat/static/js/runtime-main.c115f37a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.af9c2624e4c7563055ac8aef9a1dd615.js"
+  "/my_snapchat/precache-manifest.14300d18192eeb207539a184fc6e3806.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my_snapchat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
